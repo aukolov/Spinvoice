d895186346e7229b58035482d9c33654fef080bc
aukolov <aukolov@quotix.com>
2017-04-14 21:13:41 +0300

    Browser with splitter plus new project for working with pdf

---
+++ Spinvoice.Infrastructure.Pdf/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spinvoice.Infrastructure.Pdf
+{
+    public class Class1
+    {
+    }
+}
