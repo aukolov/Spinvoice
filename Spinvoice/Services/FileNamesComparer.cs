3a7bab37a5ea1e4043060cd1ad67c1701de2779b
aukolov <aukolov@quotix.com>
2017-04-15 15:27:35 +0300

    Displaying files and folders in same order like in Windows Explorer

---
+++ Spinvoice/Services/FileNamesComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Spinvoice.Services
+{
+    public class FileNamesComparer : IComparer<string>
+    {
+        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
+        static extern int StrCmpLogicalW(string x, string y);
+
+        public int Compare(string x, string y)
+        {
+            return StrCmpLogicalW(x, y);
+        }
+
+    }
+}
--- Spinvoice/Services/FileService.cs
+++ Spinvoice/Services/FileService.cs
@@ -1,18 +1,26 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Spinvoice.Services
 {
     public class FileService : IFileService
     {
+        private readonly FileNamesComparer _fileNamesComparer;
+
+        public FileService()
+        {
+            _fileNamesComparer = new FileNamesComparer();
+        }
+
         public IEnumerable<string> GetSubDirectories(string directoryPath)
         {
-            return Directory.GetDirectories(directoryPath);
+            return Directory.GetDirectories(directoryPath).OrderBy(s => s, _fileNamesComparer);
         }
 
         public IEnumerable<string> GetFiles(string directoryPath)
         {
-            return Directory.GetFiles(directoryPath);
+            return Directory.GetFiles(directoryPath).OrderBy(s => s, _fileNamesComparer);
         }
 
         public bool FileExists(string filePath)
