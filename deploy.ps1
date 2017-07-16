5f233e971766fd2c4d8b9b8eb293353cc411a534
aukolov <aukolov@quotix.com>
2017-07-16 15:59:48 +0300

    Auto-increment version

--- deploy.ps1
+++ deploy.ps1
@@ -2,17 +2,17 @@ $git = $env:ProgramFiles + "\Git\bin\git.exe"
 $msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
 $publish = "Spinvoice\publish\"
 
-$new_csproj = Get-Content Spinvoice.csproj |
- foreach { $n = [regex]::match($_,'(?<=\<ApplicationRevision\>)(\d+)(?=\<\/ApplicationRevision\>)').groups[1].value;
- if ($n) {$_ -replace "$n", ([int32]$n+1)} else {$_}; }
-Set-Content Spinvoice.csproj $new_csproj -encoding UTF8
-
 & $git checkout master
 & $git pull
 & $git merge dev
 
 If (Test-Path $publish) { Remove-Item $publish -Force -Recurse }
 & $msbuild Spinvoice.sln /t:publish /p:Configuration=Release /p:PublishDir=publish\
+
+$new_csproj = Get-Content Spinvoice.csproj |
+ foreach { $n = [regex]::match($_,'(?<=\<ApplicationRevision\>)(\d+)(?=\<\/ApplicationRevision\>)').groups[1].value;
+ if ($n) {$_ -replace "$n", ([int32]$n+1)} else {$_}; }
+Set-Content Spinvoice.csproj $new_csproj -encoding UTF8
 
 & $git add .
 & $git commit -m "New release"
