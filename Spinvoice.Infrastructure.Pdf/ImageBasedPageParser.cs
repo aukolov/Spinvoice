9dbc7569acac488b1884e346ed2f5f53592df666
aukolov <aukolov@quotix.com>
2017-07-23 22:09:08 +0300

    Parse PDF files with images #7 - scaling

--- Spinvoice.Infrastructure.Pdf/ImageBasedPageParser.cs
+++ Spinvoice.Infrastructure.Pdf/ImageBasedPageParser.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
 using iTextSharp.text.pdf;
 using Spinvoice.Domain.Pdf;
 using Tesseract;
@@ -30,31 +29,48 @@ namespace Spinvoice.Infrastructure.Pdf
                 return new List<List<SentenceModel>>();
             }
 
-            var page = engine.Process(new Bitmap(image));
-            var resultIterator = page.GetIterator();
+            var scale = 1d;
+            if (image.Height != 0 && image.Width != 0)
+            {
+                var widthScale = PdfPageSize.Width / image.Width;
+                var heightScale = PdfPageSize.Height / image.Height;
+
+                scale = Math.Min(widthScale, heightScale);
+            }
+
+            Page page;
+            try
+            {
+                page = engine.Process(new Bitmap(image));
+            }
+            catch (ArgumentException)
+            {
+                return new List<List<SentenceModel>>();
+            }
 
             var bricks = new List<IBrick>();
+            var resultIterator = page.GetIterator();
             while (resultIterator.Next(IteratorLevel))
             {
                 Rect rect;
                 if (resultIterator.TryGetBoundingBox(IteratorLevel, out rect))
                 {
-                    var rectText = $"{rect.X1} x {rect.Y1}, {rect.Width} x {rect.Height}";
                     var text = resultIterator.GetText(IteratorLevel);
-                    Console.WriteLine($"'{text}' - {rectText}");
+                    //var rectText = $"{rect.X1} x {rect.Y1}, {rect.Width} x {rect.Height}";
+                    //Console.WriteLine($"'{text}' - {rectText}");
                     bricks.Add(new Brick(
                         text,
-                        rect.X1,
-                        rect.Y1,
-                        rect.Width,
-                        rect.Height));
+                        rect.X1 * scale,
+                        rect.Y1 * scale,
+                        rect.Width * scale,
+                        rect.Height * scale));
                 }
             }
 
             var sentenceModels = _bricksToSentensesTranslator.Translate(
                 new List<IBrick[]>
                 {
-                    bricks.Take(25).ToArray()
+                    bricks.ToArray()
                 });
             return sentenceModels;
         }
---
+++ Spinvoice.Infrastructure.Pdf/PdfPageSize.cs
@@ -0,0 +1,8 @@
+﻿namespace Spinvoice.Infrastructure.Pdf
+{
+    public class PdfPageSize
+    {
+        public const double Height = 842;
+        public const double Width = 595;
+    }
+}
