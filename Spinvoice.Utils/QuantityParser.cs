b41ff31caa9b8833db10373f808d44aa2d7d0e62
aukolov <aukolov@quotix.com>
2017-07-25 22:10:03 +0300

    Parse PDF files with images #7 - Small fix in quantity parser

--- Spinvoice.Utils/QuantityParser.cs
+++ Spinvoice.Utils/QuantityParser.cs
@@ -1,10 +1,12 @@
 ﻿namespace Spinvoice.Utils
 {
-    public class QuantityParser
+    public static class QuantityParser
     {
         public static bool TryParse(string text, out int quantity)
         {
-            return int.TryParse(text.Replace(",", ""), out quantity);
+            return int.TryParse(
+                text.Replace(",", "").Replace(".", ""),
+                out quantity);
         }
     }
 }
