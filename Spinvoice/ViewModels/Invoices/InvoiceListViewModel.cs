8b770a8655f2ff6497199559c8a6fa3da3fecdc1
aukolov <aukolov@quotix.com>
2017-08-06 18:19:35 +0300

    Batch invoice processing #13 - Using autofac to create InvoiceViewModel

--- Spinvoice/ViewModels/Invoices/InvoiceListViewModel.cs
+++ Spinvoice/ViewModels/Invoices/InvoiceListViewModel.cs
@@ -4,63 +4,21 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using Spinvoice.Annotations;
-using Spinvoice.Domain.Accounting;
-using Spinvoice.Domain.Company;
-using Spinvoice.Domain.Exchange;
-using Spinvoice.Domain.ExternalBook;
-using Spinvoice.Domain.InvoiceProcessing;
 using Spinvoice.Domain.Pdf;
-using Spinvoice.Domain.UI;
-using Spinvoice.QuickBooks.Account;
-using Spinvoice.QuickBooks.Item;
-using Spinvoice.Services;
 using Spinvoice.Utils;
 
 namespace Spinvoice.ViewModels.Invoices
 {
     public class InvoiceListViewModel : IInvoiceListViewModel
     {
-        private readonly IAccountsChartRepository _accountsChartRepository;
-        private readonly AnalyzeInvoiceService _analyzeInvoiceService;
-        private readonly ClipboardService _clipboardService;
-        private readonly ICompanyRepository _companyRepository;
-        private readonly IExchangeRatesRepository _exchangeRatesRepository;
-        private readonly IExternalAccountRepository _externalAccountRepository;
-        private readonly IExternalCompanyRepository _externalCompanyRepository;
-        private readonly IExternalConnectionWatcher _externalConnectionWatcher;
-        private readonly IExternalInvoiceService _externalInvoiceService;
-        private readonly IExternalItemRepository _externalItemRepository;
-        private readonly TrainStrategyService _trainStrategyService;
-        private readonly IWindowManager _windowManager;
+        private readonly Func<PdfModel, PdfXrayViewModel, InvoiceViewModel> _invoiceViewModelFactory;
         private InvoiceViewModel _lastActive;
 
         public InvoiceListViewModel(
-            ICompanyRepository companyRepository,
-            IExchangeRatesRepository exchangeRatesRepository,
-            IExternalItemRepository externalItemRepository,
-            IAccountsChartRepository accountsChartRepository,
-            ClipboardService clipboardService,
             PdfModel pdfModel,
-            AnalyzeInvoiceService analyzeInvoiceService,
-            TrainStrategyService trainStrategyService,
-            IExternalInvoiceService externalInvoiceService,
-            IExternalCompanyRepository externalCompanyRepository,
-            IExternalAccountRepository externalAccountRepository,
-            IExternalConnectionWatcher externalConnectionWatcher,
-            IWindowManager windowManager)
+            Func<PdfModel, PdfXrayViewModel, InvoiceViewModel> invoiceViewModelFactory)
         {
-            _companyRepository = companyRepository;
-            _exchangeRatesRepository = exchangeRatesRepository;
-            _externalItemRepository = externalItemRepository;
-            _accountsChartRepository = accountsChartRepository;
-            _clipboardService = clipboardService;
-            _analyzeInvoiceService = analyzeInvoiceService;
-            _trainStrategyService = trainStrategyService;
-            _externalInvoiceService = externalInvoiceService;
-            _externalCompanyRepository = externalCompanyRepository;
-            _externalAccountRepository = externalAccountRepository;
-            _externalConnectionWatcher = externalConnectionWatcher;
-            _windowManager = windowManager;
+            _invoiceViewModelFactory = invoiceViewModelFactory;
             InvoiceViewModels = new ObservableCollection<InvoiceViewModel>();
             PdfXrayViewModel = pdfModel != null ? new PdfXrayViewModel(pdfModel) : null;
             AddInvoiceViewModel(pdfModel, PdfXrayViewModel);
@@ -75,21 +33,9 @@ namespace Spinvoice.ViewModels.Invoices
 
         private void AddInvoiceViewModel(PdfModel pdfModel, PdfXrayViewModel pdfXrayViewModel)
         {
-            var invoiceViewModel = new InvoiceViewModel(
-                _companyRepository,
-                _exchangeRatesRepository,
-                _externalItemRepository,
-                _accountsChartRepository,
-                _clipboardService,
+            var invoiceViewModel = _invoiceViewModelFactory(
                 pdfModel,
-                pdfXrayViewModel,
-                _analyzeInvoiceService,
-                _trainStrategyService,
-                _externalInvoiceService,
-                _externalCompanyRepository,
-                _externalAccountRepository,
-                _externalConnectionWatcher,
-                _windowManager);
+                pdfXrayViewModel);
             invoiceViewModel.Activated.Subscribe(OnActivated);
             InvoiceViewModels.Add(invoiceViewModel);
             invoiceViewModel.IsActive = true;
