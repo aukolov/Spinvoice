b6c51e16bb16073c9b017c327904b2b10cd36b6e
aukolov <aukolov@quotix.com>
2017-08-17 22:05:36 +0300

    Batch invoice processing #13 - added solution folders, introduced Spinvoice.Server.Services

---
+++ Spinvoice.Server/FileParseService.cs
@@ -0,0 +1,24 @@
+﻿using System.ServiceModel;
+
+namespace Spinvoice.Server
+{
+    [ServiceContract]
+    public interface IFileParseService
+    {
+        [OperationContract]
+        int Sum(int a, int b);
+    }
+
+    public class FileParseService : IFileParseService
+    {
+        public FileParseService()
+        {
+            
+        }
+
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
